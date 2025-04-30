5102018744ea63f4e617a615bf5aa78f19f13442
candidate-6 <4upa4el2014@gmail.com>
2025-04-30 13:24:49 +0300

    Create counter-service.py

---
+++ counter-service.py
@@ -0,0 +1,19 @@
+#!flask/bin/python
+from flask import Flask, request, request_started
+
+app = Flask(__name__)
+post_counter = 0
+get_counter = 0
+
+@app.route('/', methods=["POST", "GET"])
+def index():
+    global post_counter, get_counter
+    if request.method == "POST":
+        post_counter += 1
+        return "Hmm, Plus 1 please "
+    else:
+        get_counter += 1
+        return str(f"POST requests: {post_counter}, GET requests: {get_counter}")
+
+if __name__ == '__main__':
+    app.run(debug=True, port=443, host='0.0.0.0')
